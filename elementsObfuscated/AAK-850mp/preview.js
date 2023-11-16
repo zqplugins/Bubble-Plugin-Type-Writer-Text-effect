23a48a2a7c77daeada51a2e6f29db31f15721f2d
Андрій Пічкорський <plugins@zeroqode.com>
2023-11-16 18:58:18 +0200

    Obfuscation

---
+++ elementsObfuscated/AAK-850mp/preview.js
@@ -0,0 +1,4 @@
+function(instance, properties) {
+	function preview(_0x446df8,_0x160162){}
+	return preview(instance, properties);
+}
---
+++ elementsObfuscated/AAK-850mp/reset.js
@@ -0,0 +1,4 @@
+function(instance, context) {
+	function reset(_0x38ac87,_0x187279){}
+	return reset(instance, context);
+}
